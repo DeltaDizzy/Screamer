7afeaaf91b142fa4ce792ac677e642a8fb0fa123
DeltaDizzy <EJBraswell@gmail.com>
2018-11-26 11:54:13 -0600

    Update ScreamCollider.cs

--- src/ScreamCollider.cs
+++ src/ScreamCollider.cs
@@ -8,37 +8,34 @@ namespace Screamer
 {
     public class ScreamCollider
     {
-        private static Collider[] colArray = UnityEngine.Object.FindObjectsOfType<Collider>();
-        private static Collider col;
-        private static ScreamCollider _trigger = new ScreamCollider();
-        public static Boolean colContact;
+        private static Collider[] colArray = UnityEngine.Object.FindObjectsOfType<Collider>(); //Fill array with all colliders
+        private static Collider col; //Collider reference
+        private static ScreamCollider _trigger = new ScreamCollider(); //accessor
+        public static Boolean colContact; //contact var
 
-        public static ScreamCollider Trigger
+        public static ScreamCollider Trigger //acessor
         {
             get
             {
                 return _trigger;
             }
         }
-        public void OnTriggerEnter(Collider other)
+        public void OnTriggerEnter(Collider other //Called when trigger is entered
         {
-            if (other.attachedRigidbody == null && other.CompareTag("Screamer") && other.isTrigger)
+            if (other.CompareTag("Screamer") && other.isTrigger) //if collider is tagged "Screamer" and is a trigger
             {
-                colContact = true;
+                colContact = true; //we are contacting the trigger
             }
         }
-        public void OnTriggerExit(Collider other)
+        public void OnTriggerExit(Collider other) //collider exit
         {
-            colContact = false;
+            colContact = false; //we are not touching the collider
         }
-
-        public static Collider GetCollider(string name)
+        public static Collider GetCollider(string name) //Get collider of a given name
         {
-
-            List<Collider> cols = new List<Collider>(colArray);
-            col = cols.Find(n => n.name == Scream.triggerName);
-            return col;
-
+            List<Collider> cols = new List<Collider>(colArray); //list of colliders, same as array
+            col = cols.Find(n => n.name == Scream.triggerName); //find collider with given name
+            return col; //return the collider
         }
     }
 }
